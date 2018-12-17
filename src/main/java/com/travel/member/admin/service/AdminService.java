be0958935be594e1de7ee5244cb58101694237d2
smart-202-10 <smart-202-10@smart-202-10-PC>
2018-12-17 10:36:12 +0900

    [복영수] 관리자리스트 페이징 수정
    1. AdminService.java 수정
    
    2. CompanyController.java 수정
    
    3. PageMaker.java 수정

--- src/main/java/com/travel/member/admin/service/AdminService.java
+++ src/main/java/com/travel/member/admin/service/AdminService.java
@@ -60,19 +60,26 @@ public class AdminService {
         pageMaker.setStartPage();
         pageMaker.setEndPage();
         // 이전 페이지와 다음 페이지를 컨트롤하는 조건문
-        if(pageMaker.getCurrentBlock() == pageMaker.getLastBlock() && pageMaker.getAllCount()<100) {
-        	  pageMaker.setPrevPage(false);
-              pageMaker.setNextPage(false);
   
-        }else if(pageMaker.getCurrentBlock() == pageMaker.getLastBlock() && pageMaker.getAllCount()>100){
-            	pageMaker.setPrevPage(true);
-                pageMaker.setNextPage(false);        	
-        }else if(pageMaker.getCurrentBlock() < pageMaker.getLastBlock()&& pageMaker.getAllCount()<100) {
-            pageMaker.setPrevPage(false);
+        if(pageMaker.getCurrentBlock() != pageMaker.getLastBlock() && pageMaker.getCurrentBlock() >1){
+            System.out.println("true<>true");
+        	pageMaker.setPrevPage(true);
             pageMaker.setNextPage(true);
-        } else if(pageMaker.getCurrentBlock() < pageMaker.getLastBlock() && pageMaker.getAllCount()>100){
-            pageMaker.setPrevPage(true);
+  
+        }else  if(pageMaker.getCurrentBlock() != pageMaker.getLastBlock() && pageMaker.getLastBlock() != 1) {
+            System.out.println("false<>true");
+        	pageMaker.setPrevPage(false);
             pageMaker.setNextPage(true);
+      	
+        }else if(pageMaker.getCurrentBlock() <= pageMaker.getLastBlock() && pageMaker.getCurrentBlock() != 1){
+            System.out.println("true<>false");	
+        	pageMaker.setPrevPage(true);
+            pageMaker.setNextPage(false);   
+        	
+        }else if(pageMaker.getLastBlock() == 1) {
+        	System.out.println("false<>false");  
+        	pageMaker.setPrevPage(false);
+            pageMaker.setNextPage(false); 
         }
         System.out.println(pageMaker.getLastBlock()+"<마지막블럭");
         System.out.println(pageMaker.getCurrentBlock()+"<현재블럭");
